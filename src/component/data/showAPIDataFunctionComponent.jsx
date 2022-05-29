abdabe1951fd0d7d0654dedf0122cdbdd863a4ac
Prabhat11 <prabhat11.ranjan@gmail.com>
2022-05-29 17:15:06 +0530

    till this commit react functionality has been completed

---
+++ src/component/data/showAPIDataFunctionComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function showAPIDataFunctionComponent() {
+  return (
+      useEffect(() => {
+        return () => {
+          effect
+        };
+      }, [input]);
+    <div>
+      
+    </div>
+  );
+}
